f88ab6e6e488b8d050da1457aa2e12a63ae55a42
jegc <jegc@jumay.biz>
2018-04-12 16:48:43 -0600

    changes for code review

--- src/test/java/com/interactuamovil/apps/contactosms/api/sdk/ScheduledMessagesTest.java
+++ src/test/java/com/interactuamovil/apps/contactosms/api/sdk/ScheduledMessagesTest.java
@@ -94,7 +94,7 @@ public class ScheduledMessagesTest extends TestCase {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
-
+    
 
     /**
      * Test of add method, of class ScheduledMessages.
